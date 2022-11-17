0aa0e2e7a8c55ea8e4e08cfd848c6964602d3f79
Mauro Cicolella <mcicolella@libero.it>
2022-11-17 08:35:45 +0100

    Add batch-processing example

---
+++ batch-processing/src/main/java/net/emmecilab/jpa_examples/model/Person.java
@@ -0,0 +1,53 @@
+package net.emmecilab.jpa_examples.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.TableGenerator;
+
+@Entity
+public class Person {
+	@Id
+	//@GeneratedValue
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	//@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "person_generator")
+	//@SequenceGenerator(name="person_generator", sequenceName = "person_seq", allocationSize=50)
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	//@GeneratedValue(strategy = GenerationType.TABLE, generator = "table-generator")
+	//@TableGenerator(name = "table-generator", table = "dep_ids",
+		//pkColumnName = "seq_id", valueColumnName = "seq_value", allocationSize = 10)
+	private Long id;
+	private String firstName;
+	private String lastName;
+
+	public Person() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
